6b1c20d8ffbe559067c2da9c87bba2dca9efe319
diki-formulatrix <ahmad.apriansyah@m3.formulatrix.com>
2026-06-12 10:42:12 +0700

    feat: Create header, sidebar, and masterlayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
+import MasterLayout from './layout/MasterLayout';
 import HomePage from './pages/HomePage';
 import ArticleDetailPage from './pages/ArticleDetailPage';
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/article/:id" element={<ArticleDetailPage />} />
+      <Route element={<MasterLayout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/article/:id" element={<ArticleDetailPage />} />
+      </Route>
     </Routes>
   );
 }
